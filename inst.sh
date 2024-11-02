9d1759a726dfe487f0d9f6625d534c32266c5540
TThai <163569473+Triutheis@users.noreply.github.com>
2024-11-02 22:49:55 +0700

    Update inst.sh

--- inst.sh
+++ inst.sh
@@ -2,11 +2,11 @@ clear
 echo "Đang cài đặt"
 apt update && apt upgrade -y
 clear
-cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && pkg install wget -y && wget https://github.com/Triutheis/v4neko/raw/main/v4neko.zip && pkg install unzip v4neko.zip && chmod a+x neko && chmod a+x tun && chmod a+x psiphon-tunnel-core && chmod a+x qexit && chmod a+x wl && chmod a+x ver && chmod a+x ver2 && chmod a+x v4man && chmod a+x qoa && chmod a+x v4a
+cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && rm v4.zip && pkg install wget -y && wget https://github.com/Triutheis/v4neko/raw/main/v4neko.zip && unzip v4neko.zip && chmod a+x tun && chmod a+x psiphon-tunnel-core && chmod a+x qexit && chmod a+x wl && chmod a+x ver && chmod a+x ver2 && chmod a+x v4man && chmod a+x qoa && chmod a+x v4a
 clear
 cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/etc/ && pkg install wget -y && wget https://github.com/Triutheis/v4neko/raw/main/t1 && mv t1 motd
 clear
-cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && pkg install wget -y && wget https://raw.githubusercontent.com/Triutheis/v4neko/refs/heads/main/l1 && chmod +x * && mv l1 login &&
+cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && pkg install wget -y && wget https://github.com/Triutheis/v4neko/raw/main/l1 && chmod +x * && mv l1 login &&
 clear
 echo "Đã cài đặt thành công"
 login
